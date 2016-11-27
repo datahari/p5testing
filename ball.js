368eeecf66791feb993db6e1ad0e6de345495077
datahari <info@datahari.fi>
2016-11-27 20:56:35 +0200

    done some refactoring

--- ball.js
+++ ball.js
@@ -1,54 +1,72 @@
 function Ball() {
 
-	this.r = random(20,100);
+	this.radius   = random( 20, 100 );
+	this.diameter = this.radius * 2;
 
-	this.y = random(height/2);
-	this.x = random(this.r, width-this.r);
+	this.y = random( height / 2 );
+	this.x = random( this.radius, width - this.radius );
 
-	this.color = [random(255), random(255), random(255)];
+	this.color = [random( 255 ), random( 255 ), random( 255 )];
 
-	this.gravity = 1 * (this.r/100);
+	this.gravity   = 1 * ( this.radius / 100 );
 	this.yvelocity = 0;
-	this.xvelocity = random(1,5);
+	this.xvelocity = random( 1, 5 );
 
-	this.squish = 1;
+	this.squish   = 1;
 	this.squishig = false;
 
 	this.draw = function() {
-		this.yvelocity += this.gravity;
-		this.y += this.yvelocity;
-		this.x += this.xvelocity;
 
-		fill(this.color[0], this.color[1], this.color[2], 200);
-		ellipse(this.x, this.y, this.r*2*this.squish, this.r*2/this.squish);
+		this.yvelocity += this.gravity;
+		this.y         += this.yvelocity;
+		this.x         += this.xvelocity;
+
+		fill( this.color[0], this.color[1], this.color[2], 200 );
+
+		ellipse( this.x, this.y, this.diameter * this.squish, this.diameter / this.squish );
 
 		this.bounce();
 
 		this.unsquish();
+
 	}
 
 	this.unsquish = function() {
+
 		if( this.squishing == false && this.squish != 1 ) {
 			this.squish -= 0.08;
 		}
+
+	}
+
+	this.hitsWall = function() {
+		return this.x < this.radius || this.x + this.radius > width;
+	}
+
+	this.hitsGround = function() {
+		return this.y + this.radius > height;
 	}
 
 	this.bounce = function() {
-		if( this.x < this.r || this.x + this.r > width ) {
+
+		if( this.hitsWall() ) {
 			this.xvelocity *= -1;
 		}
-		if( this.y + this.r > height ) {
+
+		if( this.hitsGround() ) {
 			if(this.squish < 1.3) {
 				if( this.squishing == false ) {
-					this.color = [random(255), random(255), random(255)];
+					this.color = [random( 255 ), random( 255 ), random( 255 )];
 				}
-				this.squish += 0.08;
+				this.squish   += 0.08;
 				this.yvelocity = 0;
 				this.squishing = true;
 			} else {
-				this.squishing = false;
-				this.yvelocity -= 25 - (this.r/50);
+				this.squishing  = false;
+				this.yvelocity -= 25 - ( this.radius / 50 );
 			}
 		}
+
 	}
+
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 var balls = [];
 
 function setup() {
-	createCanvas(700, 700);
-	frameRate(30);
+	createCanvas( 700, 700 );
+	frameRate( 30 );
 
-	for(var i = 0; i < 40; i++) {
+	for( var i = 0; i < 40; i++ ) {
 		balls[i] = new Ball();
 	}
 }
 
 function draw() {
-	background(0);
+	background( 0 );
 	
-	for(var i = 0; i < balls.length; i++) {
+	for( var i = 0; i < balls.length; i++ ) {
 		balls[i].draw();
 	}
 }
